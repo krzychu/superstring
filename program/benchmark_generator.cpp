08b56e587bd69b7fbd9dd0212e61353c0d800aa8
Krzysztof Chrobak <krzychuch@gmail.com>
2013-01-22 10:33:08 +0000

    ustaliłem benchmarki

--- program/benchmark_generator.cpp
+++ program/benchmark_generator.cpp
@@ -1,33 +1,20 @@
 #include <instance.h>
+#include <sstream>
+
+
 int main(){
   Instance I(4);
 
-  I.randomize(10, 500, 0.7);
+  I.randomize(10, 50, 0.7);
   I.save("gdb.test");
 
-  I.randomize(5, 5000, 0.7);
-  I.save("test_5_5000_0.7.test");
-
-  I.randomize(10, 5000, 0.8);
-  I.save("test_10_5000_0.8.test");
-
-  I.randomize(50, 4000, 0.8);
-  I.save("test_50_4000_0.8.test");
-
-  I.randomize(50, 50000, 0.8);
-  I.save("test_50_50000_0.8.test");
-
-  I.randomize(100, 6500, 0.4);
-  I.save("test_100_6500_0.4.test");
-
-  I.randomize(200, 40000, 0.8);
-  I.save("test_200_40000_0.8.test");
-
-  I.randomize(400, 40000, 0.8);
-  I.save("test_400_40000_0.8.test");
-
-  I.randomize(1000, 65000, 0.8);
-  I.save("test_1000_65000_0.8.test");
-
+  for(int num_words = 10; num_words <= 100; num_words += 10){
+    for(int len = num_words * 10; len <= num_words * 100; len += num_words * 10){
+      I.randomize(num_words, len, 0.7);
+      std::stringstream fname;
+      fname << "test_" << num_words << "_" << len << ".test";
+      I.save(fname.str().c_str());
+    }
+  }
   return 0;
 }
