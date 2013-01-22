01f1581b65220785c701ad504758f508cb28858d
Krzysztof Chrobak <krzychuch@gmail.com>
2013-01-22 00:01:20 +0000

    napisałem heurezę

--- program/src/heuristic.cpp
+++ program/src/heuristic.cpp
@@ -1,5 +1,84 @@
 #include <heuristic.h>
+#include <text.h>
+#include <find_union.h>
 
-int Heuristic::run() const{
-  return 0; 
+#include <algorithm>
+#include <vector>
+
+
+struct Overlap
+{
+  int len;
+  int left;
+  int right;
+
+  Overlap(int o, int l, int r)
+    : len(o), left(l), right(r) {}
+
+  bool operator<(const Overlap & other) const
+  {
+    return len < other.len; 
+  }
+};
+
+
+
+int Heuristic::run() const
+{
+  const int n = instance_.num_words();
+  std::vector<Overlap> overlaps;
+
+  std::vector<bool> inside(n, false);
+  std::vector<bool> prefix_used(n, false);
+  std::vector<bool> suffix_used(n, false);
+  std::vector<int> next(n, -1);
+
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      if(i == j)
+        continue;
+        
+      const Instance::Word & first = instance_[i];
+      const Instance::Word & second = instance_[j];
+      std::pair<int, int> res = find_with_overlap(
+        second.begin(), second.end(),
+        instance_.pi(j),
+        first.begin(), first.end()
+      );  
+
+      if(res.first == (int)second.size()){
+        inside[j] = true;
+      }
+     
+      overlaps.push_back(Overlap(res.second, i, j));
+    }
+  }
+
+  std::sort(overlaps.begin(), overlaps.end());
+  FindUnion fu(n);
+  int size = 0;
+
+
+  while(!overlaps.empty()){
+    Overlap cur = overlaps.back();
+    overlaps.pop_back();
+
+    if(prefix_used[cur.right] || suffix_used[cur.left]);
+      continue;
+   
+    if(inside[cur.right] || inside[cur.left])
+      continue;
+
+    if(fu.find(cur.right) == fu.find(cur.left))
+      continue;
+
+    prefix_used[cur.right] = true;    
+    suffix_used[cur.left] = true;
+    next[cur.left] = cur.right;
+    fu.sum(cur.left, cur.right);
+    size += instance_[cur.left].size() - cur.len;
+  }
+  
+
+  return size; 
 }
