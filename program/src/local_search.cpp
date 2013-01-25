2094ba0fe961e406c0a588e09b468df74c026ebc
Krzysztof Chrobak <krzychuch@gmail.com>
2013-01-25 10:17:17 +0000

    działa ocenianie osobników w czasie stałym

--- program/src/local_search.cpp
+++ program/src/local_search.cpp
@@ -17,17 +17,25 @@ LocalSearch::Result TranspositionSearch::operator()
 {
   Result result(*a, 0);
   const int n = a->size();
+
+  using std::vector;
+  vector<vector<int> > res = instance_.evaluate_all_transpositions(a);
+
+  std::pair<int, int> best;
+  int best_cost = a->cost();
+
   for(int i = 0; i < n; i++){
     for(int j = i + 1; j < n; j++){
-      Individual candidate(*a);  
-      candidate.swap(i, j);
-      int cost = instance_.evaluate(&candidate);
-      candidate.set_cost(cost);
-      if(cost < result.individual.cost())
-        result.individual = candidate;
+      if(res[i][j] < best_cost){
+        best = std::make_pair(i, j);
+        best_cost = res[i][j];
+      }
       result.num_processed++;
     }
   }
+
+  result.individual.swap(best.first, best.second);
+
   return result;
 }
 
