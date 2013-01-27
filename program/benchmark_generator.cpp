9072b4a8f3652ffc00af421b3ff519e051d6eb10
Krzysztof Chrobak <krzychuch@gmail.com>
2013-01-27 18:07:22 +0000

    skończyłem raport

--- program/benchmark_generator.cpp
+++ program/benchmark_generator.cpp
@@ -8,13 +8,12 @@ int main(){
   I.randomize(10, 50, 0.7);
   I.save("gdb.test");
 
-  for(int num_words = 10; num_words <= 100; num_words += 10){
-    for(int len = num_words * 10; len <= num_words * 100; len += num_words * 10){
-      I.randomize(num_words, len, 0.7);
-      std::stringstream fname;
-      fname << "test_" << num_words << "_" << len << ".test";
-      I.save(fname.str().c_str());
-    }
+  for(int num_words = 100; num_words <= 1000; num_words += 100){
+    const int len = 50000;
+    I.randomize(num_words, len, 0.7);
+    std::stringstream fname;
+    fname << "test_" << num_words << "_" << len << ".test";
+    I.save(fname.str().c_str());
   }
   return 0;
 }
